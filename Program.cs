0c42e635ff59b7fa2cb73152470885e6f5a28add
naruru12 <90431938+naruru12@users.noreply.github.com>
2023-04-20 11:28:06 +0900

    test up

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test122343454225
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            string s = Console.ReadLine();
+        Console.WriteLine(s);
+            Console.WriteLine(s);
+        }
+}
+    }
+
